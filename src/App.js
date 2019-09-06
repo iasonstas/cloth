c9d4179bfffd479589b126f79dee0994cfb50af8
Iason Stasinos <iasonstas@gmail.com>
2019-09-06 07:58:59 +0300

    Update menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import Homepage from "./pages/homepage/homepage.component";
 import "./App.css";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATSPAGE</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <Homepage />
+      {/* The moment switch sees smthing(path) match- It renders only that and stops */}
+      <Switch>
+        <Route exact path="/" component={Homepage} />{" "}
+        {/* Exact={true} or false It means the path we see exactly. Dont stop at the first*/}
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,8 +45,9 @@ export default class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {/*  title, imageUrl, size, linkUrl = {...otherSectionProps} */}
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
