0776100933e5e3f9b2cfb04857a14b967abf0084
Iason Stasinos <38857895+iasonstas@users.noreply.github.com>
2019-09-16 08:39:56 +0300

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,15 +3,11 @@ import "firebase/firestore";
 
 import "firebase/auth";
 
-const config = {
-  apiKey: "AIzaSyAtE7jllvC8s8eXwA_F7A90doQjFWySGRg",
-  authDomain: "crwn-db-1f307.firebaseapp.com",
-  databaseURL: "https://crwn-db-1f307.firebaseio.com",
-  projectId: "crwn-db-1f307",
-  storageBucket: "",
-  messagingSenderId: "238046118807",
-  appId: "1:238046118807:web:3f0862e2652c6cb7e9bad2"
-};
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+_ CONFIG _
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
 firebase.initializeApp(config);
 
