9855cdd3e44307d3c7c8c2305c9f1f6768a0d02f
Iason Stasinos <iasonstas@gmail.com>
2019-09-18 07:42:08 +0300

    finishing signIn and handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./components/sign-in-and-sign-up/sign-in-and-sign-up.component";
-
-import "./App.css";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -22,9 +21,22 @@ class App extends React.Component {
 
   componentDidMount() {
     //once component calls fetch it wont call it again until mounts
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+        // console.log(this.state); //currentUser= null because it is asynchronous.
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
