35f092a2f9ce7ea896d9ad692afe64beaaeab0b7
Iason Stasinos <iasonstas@gmail.com>
2019-09-30 08:58:23 +0300

    Getting started with Redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,6 +4,15 @@ import "firebase/firestore";
 import "firebase/auth";
 
 // ~~~~~~~~~~~~~~~~~Config goes here.~~~~~~~~~~~~~~~~
+var config = {
+  apiKey: "AIzaSyAtE7jllvC8s8eXwA_F7A90doQjFWySGRg",
+  authDomain: "crwn-db-1f307.firebaseapp.com",
+  databaseURL: "https://crwn-db-1f307.firebaseio.com",
+  projectId: "crwn-db-1f307",
+  storageBucket: "crwn-db-1f307.appspot.com",
+  messagingSenderId: "238046118807",
+  appId: "1:238046118807:web:3f0862e2652c6cb7e9bad2"
+};
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
